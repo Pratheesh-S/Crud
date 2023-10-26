e1b87a5aa273a201a0efa770e02b5959dc8ccd80
Pratheesh-S <prath24680@gmail.com>
2023-10-26 16:06:42 +0530

    commiting the updated schema

--- src/main/java/com/diatoz/task1/dataValidatorPack/StudentDataValidation.java
+++ src/main/java/com/diatoz/task1/dataValidatorPack/StudentDataValidation.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.io.InputStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Set;
@@ -26,7 +27,7 @@ public class StudentDataValidation {
 
     public boolean checkTheStudentData(StudentEntity studentEntity) throws DataNotProper, JsonProcessingException {
 
-        logger.info("Inside the student  data validator");
+        logger.info("Inside the student  data validator with data " + studentEntity);
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         DateFormat customDateFormat = new SimpleDateFormat("yyyy-MM-dd");
@@ -37,7 +38,7 @@ public class StudentDataValidation {
 
         JsonSchemaFactory factory = JsonSchemaFactory.builder(JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V7)).build();
         JsonSchema schema = factory.getSchema(ClassLoader.getSystemResourceAsStream("schema/studentSchema.json"));
-
+        logger.info(schema.toString());
         Set<ValidationMessage> errors = schema.validate(studentJsonNode);
 
         if (!errors.isEmpty()) {
