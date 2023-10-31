a62e7940e34f9084ac9f6ed9274d2bdcf487cc57
Pratheesh-S <prath24680@gmail.com>
2023-10-31 10:10:29 +0530

    commit the security file

--- src/main/java/com/diatoz/task1/config/JwtConfig.java
+++ src/main/java/com/diatoz/task1/config/JwtConfig.java
@@ -1,4 +1,34 @@
 package com.diatoz.task1.config;
 
+
+import com.diatoz.task1.security.JwtAuthenticationFilter;
+import com.diatoz.task1.security.JwtSecurityEntryPoint;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
 public class JwtConfig {
+
+    @Autowired
+    private JwtSecurityEntryPoint point;
+    @Autowired
+    private JwtAuthenticationFilter filter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable).cors(AbstractHttpConfigurer::disable)
+                 .authorizeHttpRequests(auth->auth.requestMatchers("/*").authenticated().
+                         requestMatchers("/auth/login").permitAll().anyRequest().authenticated()).
+                exceptionHandling(ex -> ex.authenticationEntryPoint(point))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 }
