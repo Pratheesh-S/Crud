6e00f1d29fcbe5da98052d9fd73e1239d0914aa8
Pratheesh-S <prath24680@gmail.com>
2023-11-06 15:57:02 +0530

    commit the security file role based

--- src/main/java/com/diatoz/task1/config/JwtConfig.java
+++ src/main/java/com/diatoz/task1/config/JwtConfig.java
@@ -24,9 +24,9 @@ public class JwtConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable).cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/**").permitAll())
-                        .authorizeHttpRequests(auth->auth.requestMatchers("/student/user/**","/college/user/**")
+                        .authorizeHttpRequests(auth->auth.requestMatchers("/student/user/**","/college/user/**","/test/user")
                                 .hasRole("USER"))
-                        .authorizeHttpRequests(auth->auth.requestMatchers("/student/admin/**","/college/admin/**")
+                        .authorizeHttpRequests(auth->auth.requestMatchers("/student/admin/**","/college/admin/**","/test/admin")
                                 .hasRole("ADMIN").anyRequest().authenticated()).
                         exceptionHandling(ex -> ex.authenticationEntryPoint(point))
                         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
